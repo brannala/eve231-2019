95daeefb63625a0c2d28a543da2732c85cb48183
Bruce Rannala <brannala@ucdavis.edu>
2019-09-18 01:24:45 -0700

    Added brew configuration file for mac OSX

---
+++ scripts/eve231_macOSX_postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Update and Upgrade
+echo "Installing additional software for EVE 231"
+echo "Updating brew"
+brew update
+echo "Installing latest emacs"
+brew install emacs
+echo "Installing latest python3"
+brew install python3
+echo "Installing GNU typist"
+brew install gtypist
